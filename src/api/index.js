69c48bc8f650ff1bd73033eea84b0ad329eefe5a
nisargaa20 <125184228+nisargaa20@users.noreply.github.com>
2024-04-15 20:18:17 +0530

    RapidAPI

---
+++ src/api/index.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
+
+const options = {
+ 
+  params: {
+    bl_latitude: '11.847676',
+    tr_latitude: '12.838442',
+    bl_longitude: '109.095887',
+    tr_longitude: '109.149359',
+    
+  },
+  headers: {
+    'X-RapidAPI-Key': 'bf2c34874amsh35b48ede51d7dccp139d5ajsn12a790d4f371',
+    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+  }
+};
+
+
+
+export const getPlacesData = async () => {
+  try {
+    const  {data: {data}} = await axios.get(URL, options);
+return data;
+  } catch (error) {
+    console.log(error)
+    
+  }
+}
+
